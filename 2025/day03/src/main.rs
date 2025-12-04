68766257d2b5205e65750841f0001ac6a2cf5286
Noel Chia <45459808+HelloNoel@users.noreply.github.com>
2025-12-05 00:34:12 +0100

    2025 day 3 part 2

--- 2025/day03/src/main.rs
+++ 2025/day03/src/main.rs
@@ -1,30 +1,31 @@
+use std::char;
 use utils;
 
 fn main() {
     let contents = utils::read_file();
 
-    let res1: u32 = contents
+    println!("Part 1: {}", joltage(&contents, 2));
+    println!("Part 2: {}", joltage(&contents, 12));
+}
+
+fn joltage(banks: &str, num: usize) -> u64 {
+    banks
         .lines()
         .map(|line| {
             let bank: Vec<u32> = line.chars().map(|c| c.to_digit(10).unwrap()).collect();
             let l = bank.len();
 
-            let i = l - 2 - argmax(&bank[..l - 1].iter().rev());
-            let j = argmax(&bank[i + 1..].iter());
-
-            (bank[i].to_string() + &bank[i + j + 1].to_string())
-                .parse::<u32>()
-                .unwrap()
+            let mut res = String::new();
+            let mut bank_slice = &bank[..];
+            let mut end = l - num + 1;
+            for _ in 0..num {
+                let m = bank_slice[0..end].iter().max().unwrap();
+                let p = bank_slice.iter().position(|x| x == m).unwrap();
+                res.push(char::from_digit(bank_slice[p], 10).unwrap());
+                bank_slice = &bank_slice[p + 1..];
+                end -= p;
+            }
+            res.parse::<u64>().unwrap()
         })
-        .sum();
-
-    println!("Part 1: {}", res1);
-}
-
-fn argmax(v: &(impl Iterator<Item = impl Ord + Copy> + Clone)) -> usize {
-    v.clone()
-        .enumerate()
-        .max_by_key(|&(_, val)| val)
-        .map(|(idx, _)| idx)
-        .unwrap()
+        .sum()
 }
